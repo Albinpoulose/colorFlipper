b131d776266ea893cf1c43aa398090e3088ee28a
Albin Poulose <albinfreesme@gmail.com>
2022-01-24 00:25:50 +0530

    code formatted

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 //adding action when user click the button
-document.querySelector("button").addEventListener("click",()=>{
+document.querySelector("button").addEventListener("click", () => {
     //calling funtion to generate and hexa decimal value
-    hexagenerator((hexadigit)=>{
-        
+    hexagenerator((hexadigit) => {
+
     });
 })
 
 
 //funtion to generate hexadecimal value
-function hexagenerator(){
-    
+function hexagenerator() {
+
     //array declearation with all the values of a hexadecimal code 
-    const number = [1,2,3,4,5,6,7,8,9,0,"A","B","C","D","E","F"];
-    
+    const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "A", "B", "C", "D", "E", "F"];
+
     //variable to store hexadecimal value
     let hexadigit = "#";
 
     //loop to generate six digit hexadecimal
-    for(let i=0;i<6;i++){
+    for (let i = 0; i < 6; i++) {
 
         //math.random() funtion will generate a random number between 0 to 15 
-         arrnumber =   Math.floor(Math.random()*number.length);
-         //concatinating generated number on each iteration,()
-         hexadigit = hexadigit+ number[arrnumber];
-    
-        }
-      
-     
+        arrnumber = Math.floor(Math.random() * number.length);
+        //concatinating generated number on each iteration,()
+        hexadigit = hexadigit + number[arrnumber];
+
+    }
+
+
     // changing background color 
-    document.getElementById("body").style.backgroundColor =hexadigit;
+    document.getElementById("body").style.backgroundColor = hexadigit;
     //displaying hexadigit on screen 
     document.getElementById("colordis").innerText = hexadigit;
 }
